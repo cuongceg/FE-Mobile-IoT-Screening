05cc227a71687a55e5681ef9ab0bf33aec8f87ed
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2025-03-28 23:23:58 +0700

    refactor: add custom fonts

---
+++ lib/core/decorations/fonts.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Fonts{
+  final TextStyle heading = GoogleFonts.outfit(fontSize:40,fontWeight: FontWeight.w500,color: Colors.white);
+  final TextStyle subHeading = GoogleFonts.outfit(fontSize:22,fontWeight: FontWeight.w300,color: Colors.white);
+  final TextStyle onPrimaryContainerFonts = GoogleFonts.outfit(fontSize:20,fontWeight: FontWeight.w400,color: Colors.white);
+  final TextStyle onPrimaryFonts = GoogleFonts.outfit(fontSize:16,fontWeight: FontWeight.w400,color: Colors.white);
+  final TextStyle title = GoogleFonts.outfit(fontSize:18,fontWeight: FontWeight.w500,color: Colors.white);
+  final TextStyle information = GoogleFonts.roboto(fontSize:20,fontWeight: FontWeight.w600,color: Colors.white);
+}
