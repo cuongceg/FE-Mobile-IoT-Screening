6aa4e75c9c7ce650c39fc911ad50fc27e521c612
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2025-04-03 15:48:07 +0700

    refactor: add forgot password UI

--- lib/ui/authentication/login_screen.dart
+++ lib/ui/authentication/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:speech_to_text_iot_screen/core/component/loading_animation.dart';
 import 'package:speech_to_text_iot_screen/core/decorations/button_decorations.dart';
 import 'package:speech_to_text_iot_screen/core/decorations/custom_input_decorations.dart';
+import 'package:speech_to_text_iot_screen/core/ultis/show_notify.dart';
 
 import '../../core/decorations/fonts.dart';
 import '../../providers/auth_provider.dart';
@@ -15,7 +16,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  bool isHidePassword = true;
   final _formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
@@ -36,6 +36,7 @@ class _LoginScreenState extends State<LoginScreen> {
               padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
               child: TextFormField(
                 controller: emailController,
+                textInputAction: TextInputAction.next,
                 decoration: CustomInputDecorations.inputDecoration(
                   hintText: 'UserId',
                   prefixIcon: Icon(Icons.person_2_rounded,color: colorScheme.secondary),
@@ -53,20 +54,19 @@ class _LoginScreenState extends State<LoginScreen> {
               padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
               child: TextFormField(
                 controller: passwordController,
+                textInputAction: TextInputAction.done,
                 decoration: CustomInputDecorations.inputDecoration(
                     hintText: 'Password',
                     context: context,
                     prefixIcon: Icon(Icons.lock_rounded,color: colorScheme.secondary),
                     suffixIcon: IconButton(
                         onPressed: (){
-                          setState((){
-                            isHidePassword = !isHidePassword;
-                          });
+                          authProvider.togglePasswordVisibility();
                         },
-                        icon: Icon(isHidePassword?Icons.visibility:Icons.visibility_off,color: colorScheme.secondary,)
+                        icon: Icon(authProvider.isHidePassword?Icons.visibility:Icons.visibility_off,color: colorScheme.secondary,)
                     )
                 ),
-                obscureText: isHidePassword,
+                obscureText: authProvider.isHidePassword,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please enter password';
@@ -75,26 +75,82 @@ class _LoginScreenState extends State<LoginScreen> {
                 },
               ),
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                    onPressed: (){
+                      showForgotPasswordDialog(context);
+                    },
+                    child: Text('Forgot Password?',style: Fonts().body1.copyWith(color: colorScheme.primary,fontWeight: FontWeight.w500),)
+                )
+              ],
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
               child: SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
-                  onPressed: () {
+                  onPressed: () async {
                     if (_formKey.currentState!.validate()) {
-                      authProvider.login(emailController.text, passwordController.text);
+                      await authProvider.login(emailController.text, passwordController.text);
+                    }
+                    if(authProvider.state == 'error'){
+                      ShowNotify.showSnackBar(context, authProvider.error!);
                     }
                   },
                   style: ButtonDecorations.primaryButtonStyle(context: context),
                   child: authProvider.state == 'loading'
                       ? LoadingAnimation().circleLoadingAnimation(context,color: Colors.white,size: 30)
-                      : Text('Login',style: Fonts().title.copyWith(fontWeight: FontWeight.w400),),
+                      : Text('Login',style: Fonts().title2.copyWith(color: Colors.white),),
                 ),
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void showForgotPasswordDialog(BuildContext context) async{
+    final passwordController = TextEditingController();
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Consumer<AuthProvider>(
+          builder: (context,authProvider,child){
+            return AlertDialog(
+              title: Center(child: Text('Forgot Password',style: Fonts().title1)),
+              content: TextField(
+                controller: passwordController,
+                decoration: CustomInputDecorations.inputDecoration(
+                  hintText: 'Enter your username',
+                  context: context,
+                  prefixIcon: Icon(Icons.person,color: Theme.of(context).colorScheme.secondary),
+                ),
+              ),
+              actions: [
+                Center(
+                  child: ElevatedButton(
+                    onPressed: ()async{
+                      await authProvider.forgotPassword(passwordController.text);
+                      if(authProvider.state == 'error'){
+                        ShowNotify.showSnackBar(context, authProvider.error!);
+                      }else if(authProvider.state == 'initial'){
+                        Navigator.pushNamed(context, '/login/reset_password');
+                      }
+                    },
+                    style: ButtonDecorations.primaryButtonStyle(context: context),
+                    child: authProvider.state == 'loading'
+                        ? LoadingAnimation().circleLoadingAnimation(context,color: Colors.white,size: 30)
+                        : Text('Send reset token',style: Fonts().body1.copyWith(color: Colors.white),),
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      },
     );
   }
 }
