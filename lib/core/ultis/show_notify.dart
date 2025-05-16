b5c74e13d8b35480d1ea8147dc50d53d931c8e02
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2025-05-16 11:15:12 +0700

    feat: add fluttertoast dependency and implement showToastBar method for notifications

--- lib/core/ultis/show_notify.dart
+++ lib/core/ultis/show_notify.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ShowNotify{
   static void showSnackBar(BuildContext context, String message) {
@@ -7,5 +8,17 @@ class ShowNotify{
       duration: const Duration(seconds: 2),
     );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  static void showToastBar(String message){
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.white,
+        textColor: Colors.black,
+        fontSize: 16.0
+    );
   }
 }
